9f98533d94e0228c499935656d24904fa4877999
Silvio Fricke <silvio.fricke@gmail.com>
2015-09-28 18:59:31 +0200

    btrfs-progs: use calloc instead of malloc+memset for cmds-check.c
    This patch is generated from a coccinelle semantic patch:
    
    	identifier t;
    	expression e;
    	statement s;
    	@@
    	-t = malloc(e);
    	+t = calloc(1, e);
    	(
    	if (!t) s
    	|
    	if (t == NULL) s
    	|
    	)
    	-memset(t, 0, e);
    
    Signed-off-by: Silvio Fricke <silvio.fricke@gmail.com>

--- cmds-check.c
+++ cmds-check.c
@@ -4892,13 +4892,11 @@ struct chunk_record *btrfs_new_chunk_record(struct extent_buffer *leaf,
 	ptr = btrfs_item_ptr(leaf, slot, struct btrfs_chunk);
 	num_stripes = btrfs_chunk_num_stripes(leaf, ptr);
 
-	rec = malloc(btrfs_chunk_record_size(num_stripes));
+	rec = calloc(1, btrfs_chunk_record_size(num_stripes));
 	if (!rec) {
 		fprintf(stderr, "memory allocation failed\n");
 		exit(-1);
 	}
-
-	memset(rec, 0, btrfs_chunk_record_size(num_stripes));
 
 	INIT_LIST_HEAD(&rec->list);
 	INIT_LIST_HEAD(&rec->dextents);
@@ -4997,12 +4995,11 @@ btrfs_new_block_group_record(struct extent_buffer *leaf, struct btrfs_key *key,
 	struct btrfs_block_group_item *ptr;
 	struct block_group_record *rec;
 
-	rec = malloc(sizeof(*rec));
+	rec = calloc(1, sizeof(*rec));
 	if (!rec) {
 		fprintf(stderr, "memory allocation failed\n");
 		exit(-1);
 	}
-	memset(rec, 0, sizeof(*rec));
 
 	rec->cache.start = key->objectid;
 	rec->cache.size = key->offset;
@@ -5046,12 +5043,11 @@ btrfs_new_device_extent_record(struct extent_buffer *leaf,
 	struct device_extent_record *rec;
 	struct btrfs_dev_extent *ptr;
 
-	rec = malloc(sizeof(*rec));
+	rec = calloc(1, sizeof(*rec));
 	if (!rec) {
 		fprintf(stderr, "memory allocation failed\n");
 		exit(-1);
 	}
-	memset(rec, 0, sizeof(*rec));
 
 	rec->cache.objectid = key->objectid;
 	rec->cache.start = key->offset;
